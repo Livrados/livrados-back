90b314773af2b5f671e7048179f33ce87986e3e4
Bruno Ferreira <brunoferreira1999@live.com>
2021-09-29 23:01:09 -0300

    Primeira implementação do cadastro de livros

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,11 @@ const authMiddleware = require('../middleware/auth');
 
 const loginController = require('../controller/loginController');
 const userRegistrationController = require('../controller/userRegistrationController');
+const bookRegistrationController = require('../controller/bookRegistrationController');
 
 routes.post('/login', loginController);
 routes.post('/user-registration', userRegistrationController);
+routes.post('/book-registration', bookRegistrationController);
 
 routes.get('/auth', authMiddleware.authUser);
 
